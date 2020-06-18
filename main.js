2887bd82a55494416311c1e3279023615f2c34dc
Arkadiusz Gucwa <agie@onet.com.pl>
2020-06-18 21:35:44 +0200

    BEM 2

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
-const firstName = 'Maciek';
-const age = 32;
+const firstName = 'Arek';
+const age = 31;
 
 
 
 console.log(firstName);
 console.log(age);
-console.log(`Witaj Drogi odwiedzający, nazywam się ${firstName} i mam ${age} lata.`)
+console.log(`Witaj Drogi odwiedzający, nazywam się ${firstName} i mam ${age} lat.`)
+
+const heading = document.querySelector('.main__heading--js');
+
+heading.innerHTML = `Witaj Drogi odwiedzający, nazywam się ${firstName} i mam ${age} lat.`
+
+const emptyParagraph = document.querySelector('.week-summary__description--js')
+emptyParagraph.innerHTML = `Nawet uzupełniłem treść JavaScriptem`
